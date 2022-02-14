46384be90e82e8ed210b71afc7cc293846699dcd
dabit3 <dabit3@gmail.com>
2022-02-14 14:26:12 -0600

    fixed error in ipfsuri

--- src/mapping.ts
+++ src/mapping.ts
@@ -34,7 +34,7 @@ export function handleTransfer(event: TransferEvent): void {
           token.image = image.toString()
           token.externalURL = externalURL.toString()
           token.description = description.toString()
-          token.ipfsURI = 'ipfs.io/ipfs/' + metadata.toString()
+          token.ipfsURI = 'ipfs.io/ipfs/' + ipfshash + token.tokenURI
         }
 
         const coven = value.get('coven')
